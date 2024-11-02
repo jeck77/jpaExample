4d2b6d93c292acf047b84acd319718cb21bf92e5
ajm <jeck77@naver.com>
2024-11-02 21:39:19 +0900

    예제 생성

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ORDER_ID")
+    private Long id;
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate; // ORDER_DATE, order_date
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
